73149844f6ecec0ad6a566d7f0a6092ea3a8de32
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-04-19 08:41:06 +0700

    add : new Fitur Searh

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,20 @@
-import { useState } from "react"
-const Search = () =>{
-    const [search , setSearch ] = useState("")
-    const handleSearch = (e)=>{
-        setSearch(e.target.value)
-    }
- return (
-    <>
-    
-    <div>
-        Search : <input  onChange={handleSearch} />
-    </div>
+import {useState} from "react"
+const Search = (props) => {
+	const [search, setSearch] = useState("")
 
-    <p>Ditemukan 0 data dari pencarian kata {search}</p>
-    </>
- )
+	const handleSearch = (e) => {
+		setSearch(e.target.value)
+		props.onSearchChange(e.target.value)
+	}
+	return (
+		<>
+			<div>
+				Search : <input onChange={handleSearch} />
+			</div>
+
+			<p>Ditemukan 0 data dari pencarian kata {search}</p>
+		</>
+	)
 }
 
 export default Search
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,20 @@
-import posts from '../posts.json';
+import postsData from '../posts.json';
 import Article from '../components/Article';
 import Search from '../components/Search';
+import { useState } from 'react';
 const HomePage = () =>{
-    
+    const [posts ,setPosts] =useState(postsData)
+
+    const handleFilter = (value) =>{
+        const filteredPosts = postsData.filter((item) =>(
+            item.title.toLowerCase().includes(value.toLowerCase())
+        ))
+        setPosts(filteredPosts)
+    }
 
     return(
         <div>
-            <Search/>
+            <Search onSearchChange={handleFilter}/>
             <div>
             {posts.map(({title,date,description } ,index) => (
                 <Article
